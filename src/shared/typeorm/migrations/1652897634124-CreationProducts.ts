98e98ac87ffb804f3f8da696ea2af8f53851a682
xnascimentox <rafaelnascimentodelimarnl@gmail.com>
2022-05-20 15:52:39 -0300

    Revert "conexão com DBeaver e criação de tabela com sucesso!"
    This reverts commit f508e6b391f7ffd5bc2d032a2cb64e5ad438eec7.

--- src/shared/typeorm/migrations/1652897634124-CreationProducts.ts
+++ src/shared/typeorm/migrations/1652897634124-CreationProducts.ts
@@ -3,8 +3,7 @@ import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export class CreationProducts1652897634124 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(
-        new Table({
+      await queryRunner.createTable(new Table({
         name: 'products',
         columns: [
           {
@@ -20,7 +19,7 @@ export class CreationProducts1652897634124 implements MigrationInterface {
           },
           {
             name: 'price',
-            type: 'decimal',
+            type: 'dcimal',
             precision: 10,
             scale: 2,
           },
