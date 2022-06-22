ee774d5f0ff90c7299c512f8cb1cfffa1f180698
xnascimentox <rafaelnascimentodelimarnl@gmail.com>
2022-06-22 16:45:44 -0300

    Seção 9/81

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -10,12 +10,21 @@ import passwordRouter from "@modules/users/routes/password.routes";
 
 import profileRouter from "@modules/users/routes/profile.routes";
 
+import customersRouter from "@modules/customers/routes/customers.routes";
+
 const routes = Router ();
 
 routes.use('/products', productsRouter);
+
 routes.use('/users', usersRouter);
+
 routes.use('/sessions', sessionsRouter);
+
 routes.use('/password', passwordRouter);
-routes.use('/profile', profileRouter)
+
+routes.use('/profile', profileRouter);
+
+routes.use('/customers', customersRouter);
+
 
 export default routes;
